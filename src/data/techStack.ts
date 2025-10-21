dcab6e312f0e3d7b8bd5b9350f2f05255aae16dc
hilmiyahya19 <hilmiyahya199@gmail.com>
2025-10-21 09:47:06 +0700

    add PHP & Vue.js

--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -4,15 +4,17 @@ export const techStack = [
   { id: 2, name: "CSS", image: "https://cdn.worldvectorlogo.com/logos/css-3.svg" },
   { id: 3, name: "JavaScript", image: "https://cdn.worldvectorlogo.com/logos/javascript-1.svg" },
   { id: 4, name: "TypeScript", image: "https://cdn.worldvectorlogo.com/logos/typescript.svg" },
+  { id: 5, name: "PHP", image: "https://cdn.worldvectorlogo.com/logos/php-1.svg" },
 
   // 🔹 Frontend Framework & Styling
-  { id: 5, name: "React", image: "https://cdn.worldvectorlogo.com/logos/react-2.svg" },
-  { id: 6, name: "Next.js", image: "https://cdn.worldvectorlogo.com/logos/next-js.svg" },
-  { id: 7, name: "Tailwind CSS", image: "https://cdn.worldvectorlogo.com/logos/tailwind-css-2.svg" },
+  { id: 6, name: "React", image: "https://cdn.worldvectorlogo.com/logos/react-2.svg" },
+  { id: 7, name: "Next.js", image: "https://cdn.worldvectorlogo.com/logos/next-js.svg" },
+  { id: 8, name: "Vue.js", image: "https://cdn.worldvectorlogo.com/logos/vue-9.svg" },
+  { id: 9, name: "Tailwind CSS", image: "https://cdn.worldvectorlogo.com/logos/tailwind-css-2.svg" },
 
   // 🔹 Backend & Database
-  { id: 8, name: "Node.js", image: "https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg" },
-  { id: 9, name: "Express.js", image: "https://cdn.worldvectorlogo.com/logos/express-109.svg" },
-  { id: 10, name: "MongoDB", image: "https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg" },
-  { id: 11, name: "MySQL", image: "https://cdn.worldvectorlogo.com/logos/mysql-logo-pure.svg" },
+  { id: 10, name: "Node.js", image: "https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg" },
+  { id: 11, name: "Express.js", image: "https://cdn.worldvectorlogo.com/logos/express-109.svg" },
+  { id: 12, name: "MongoDB", image: "https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg" },
+  { id: 13, name: "MySQL", image: "https://cdn.worldvectorlogo.com/logos/mysql-logo-pure.svg" },
 ];
