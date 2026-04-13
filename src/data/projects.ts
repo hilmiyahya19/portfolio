3b830f12e7e734e4713b4b1204cc28a3c24513e9
hilmiyahya19 <hilmiyahya199@gmail.com>
2026-04-13 11:36:23 +0700

    menambah 2 project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,48 +2,55 @@ export const projects = [
   {
     id: 1,
     name: 'Website Koperasi Konsumen "Mulia"',
-    description: 'Website koperasi yang terdiri dari landing page informatif dan dashboard admin untuk manajemen koperasi, dengan tiga role pengguna: user, anggota, dan admin.',
-    image: '/images/koperasi.png',
-    technologies: ["React", "Node.js","Express.js", "MongoDB", "Tailwind"],
-    demoLink: 'https://koperasi-konsumen-mulia.vercel.app/',
-    githubLinkFrontend: 'https://github.com/hilmiyahya19/frontend_koperasi-mulia',
-    githubLinkBackend: 'https://github.com/hilmiyahya19/backend_koperasi-mulia'
+    description:
+      "Website koperasi yang terdiri dari landing page informatif dan dashboard admin untuk manajemen koperasi, dengan tiga role pengguna: user, anggota, dan admin.",
+    image: "/images/koperasi.png",
+    technologies: ["React", "Node.js", "Express.js", "MongoDB", "Tailwind"],
+    demoLink: "https://koperasi-konsumen-mulia.vercel.app/",
+    githubLinkFrontend:
+      "https://github.com/hilmiyahya19/frontend_koperasi-mulia",
+    githubLinkBackend: "https://github.com/hilmiyahya19/backend_koperasi-mulia",
   },
   {
     id: 2,
-    name: 'Movie App',
-    description: 'Sistem informasi movie menggunakan TMDb API yang menampilkan film beserta detailnya berdasarkan kategori.',
+    name: "Movie App",
+    description:
+      "Sistem informasi movie menggunakan TMDb API yang menampilkan film beserta detailnya berdasarkan kategori.",
     image: "/images/movie-app.png",
     technologies: ["React"],
-    demoLink: 'https://movie-database-hilmiyahya19.vercel.app/',
-    githubLink: 'https://github.com/hilmiyahya19/0110221152_TI06_FRONTEND_14'
+    demoLink: "https://movie-database-hilmiyahya19.vercel.app/",
+    githubLink: "https://github.com/hilmiyahya19/0110221152_TI06_FRONTEND_14",
   },
   {
     id: 3,
-    name: 'Covid App',
-    description: 'Website untuk manajemen data pasien covid.',
+    name: "Covid App",
+    description: "Website untuk manajemen data pasien covid.",
     image: "/images/covid-app.png",
     technologies: ["React"],
-    demoLink: 'https://hilmiyahya19-covid-app.vercel.app/',
-    githubLink: 'https://github.com/hilmiyahya19/frontend-programming-final-project-uas'
+    demoLink: "https://hilmiyahya19-covid-app.vercel.app/",
+    githubLink:
+      "https://github.com/hilmiyahya19/frontend-programming-final-project-uas",
   },
   {
     id: 4,
-    name: 'Howheal shoes and care',
-    description: 'Website cleaning service sepatu yang dirancang untuk memudahkan manajemen data pelanggan. Proyek ini dikembangkan secara berkelompok dalam program Massive Project pada studi independen di Infinite Learning.',
+    name: "Howheal shoes and care",
+    description:
+      "Website cleaning service sepatu yang dirancang untuk memudahkan manajemen data pelanggan. Proyek ini dikembangkan secara berkelompok dalam program Massive Project pada studi independen di Infinite Learning.",
     image: "/images/howheal.png",
     technologies: ["React", "Express.js"],
-    demoLink: 'https://github.com/rakhazan/massive-infinite-loop',
-    githubLink: 'https://github.com/rakhazan/massive-infinite-loop-api'
+    demoLink: "https://github.com/rakhazan/massive-infinite-loop",
+    githubLink: "https://github.com/rakhazan/massive-infinite-loop-api",
   },
   {
     id: 5,
-    name: 'Peduli Pintar',
-    description: 'Peduli Pintar merupakan platform donasi yang dikembangkan dalam Capstone Project Studi Independen di Alterra Academy pada program Frontend Engineer with React JS (untuk divisi React). Proyek ini dibangun secara kolaboratif oleh tim yang terdiri dari divisi React, Flutter, UI/UX, Golang, Data Engineer, dan Quality Assurance. Dalam proyek ini, saya bertanggung jawab sebagai pemimpin divisi React dengan peran utama mengembangkan landing page dan dashboard admin untuk mengelola data donasi, relawan, artikel, organisasi, pengguna, serta transaksi, sekaligus mengintegrasikan visualisasi data untuk kebutuhan analisis dan pelaporan.',
+    name: "Peduli Pintar",
+    description:
+      "Peduli Pintar merupakan platform donasi yang dikembangkan dalam Capstone Project Studi Independen di Alterra Academy pada program Frontend Engineer with React JS (untuk divisi React). Proyek ini dibangun secara kolaboratif oleh tim yang terdiri dari divisi React, Flutter, UI/UX, Golang, Data Engineer, dan Quality Assurance. Dalam proyek ini, saya bertanggung jawab sebagai pemimpin divisi React dengan peran utama mengembangkan landing page dan dashboard admin untuk mengelola data donasi, relawan, artikel, organisasi, pengguna, serta transaksi, sekaligus mengintegrasikan visualisasi data untuk kebutuhan analisis dan pelaporan.",
     image: "/images/peduli-pintar.png",
     technologies: ["React", "Tailwind"],
-    demoLink: 'https://pedulipintar.vercel.app/',
-    githubLink: 'https://github.com/Kelompok-5-Capstone-Alterra-2024/Frontend-React'
+    demoLink: "https://pedulipintar.vercel.app/",
+    githubLink:
+      "https://github.com/Kelompok-5-Capstone-Alterra-2024/Frontend-React",
   },
   {
     id: 6,
@@ -53,5 +60,24 @@ export const projects = [
     technologies: ["Next.js", "TypeScript", "Tailwind", "Firebase"],
     demoLink: "https://my-next-app-router-hy.vercel.app/",
     githubLink: "https://github.com/hilmiyahya19/aniinfo",
+  },
+  {
+    id: 7,
+    name: "Football players app",
+    description:
+      "Website untuk pembelajaran Laravel berisi CRUD data pemain sepak bola.",
+    image: "/images/football-players-app.png",
+    technologies: ["Laravel", "Tailwind"],
+    demoLink: "",
+    githubLink: "https://github.com/hilmiyahya19/tutorial-laravel12-dasar",
+  },
+  {
+    id: 8,
+    name: "Surya Figs Garden",
+    description: "Landing page penjualan tanaman.",
+    image: "/images/surya-figs-garden.png",
+    technologies: ["Next.js", "TypeScript", "Tailwind"],
+    demoLink: "https://suryafigsgarden.vercel.app/",
+    githubLink: "https://github.com/hilmiyahya19/surya-figs-garden",
   },
 ];
